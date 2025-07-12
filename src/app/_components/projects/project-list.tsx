efd81ae64fd520027d0abe15a482c950f3d68189
andersonkaiti <anderkaiti@gmail.com>
2025-07-12 12:33:40 -0300

    :recycle: refactor: use Section components in ProjectList and Technologies for improved layout and consistency

--- src/app/_components/projects/project-list.tsx
+++ src/app/_components/projects/project-list.tsx
@@ -1,3 +1,9 @@
+import {
+  SectionContainer,
+  SectionHeader,
+  SectionSubtitle,
+  SectionTitle,
+} from '@components/ui/section'
 import { getProjects } from '@services/projects.service'
 import { Project } from './project'
 
@@ -5,17 +11,18 @@ export async function ProjectList() {
   const projects = await getProjects()
 
   return (
-    <section className="w-full space-y-8 px-6 md:space-y-16">
-      <header>
-        <h1 className="scroll-m-20 text-balance text-center font-bold text-4xl tracking-wide">
-          Projetos
-        </h1>
-      </header>
+    <SectionContainer>
+      <SectionHeader>
+        <SectionTitle>Projetos</SectionTitle>
+        <SectionSubtitle>
+          Alguns dos projetos que desenvolvi ao longo da minha jornada
+        </SectionSubtitle>
+      </SectionHeader>
       <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {projects.map((project) => (
           <Project key={project.id} {...project} />
         ))}
       </div>
-    </section>
+    </SectionContainer>
   )
 }
--- src/app/_components/technologies/technologies.tsx
+++ src/app/_components/technologies/technologies.tsx
@@ -1,19 +1,26 @@
+import {
+  SectionContainer,
+  SectionHeader,
+  SectionSubtitle,
+  SectionTitle,
+} from '@components/ui/section'
 import { stack } from '../../_constants/stack'
 import { Stack } from './stack'
 
 export function Technologies() {
   return (
-    <section className="space-y-8 px-6 md:space-y-16">
-      <header>
-        <h1 className="scroll-m-20 text-balance text-center font-bold text-4xl tracking-wide">
-          Principais Tecnologias
-        </h1>
-      </header>
+    <SectionContainer>
+      <SectionHeader>
+        <SectionTitle>Principais Tecnologias</SectionTitle>
+        <SectionSubtitle>
+          Ferramentas e tecnologias que domino para criar soluções completas
+        </SectionSubtitle>
+      </SectionHeader>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {stack.map((skill) => (
           <Stack key={skill.id} {...skill} />
         ))}
       </div>
-    </section>
+    </SectionContainer>
   )
 }
