7349c4ac9dfe3568f7e2c988eac7038422ef9bc6
andersonkaiti <anderkaiti@gmail.com>
2025-07-14 22:46:59 -0300

    :lipstick: style: enhance loading skeleton styles with new color variables for improved visual consistency

--- src/app/@certifications/_components/loading-skeleton.tsx
+++ src/app/@certifications/_components/loading-skeleton.tsx
@@ -4,24 +4,24 @@ import { Skeleton } from '@components/ui/skeleton'
 
 export function LoadingSkeleton() {
   return (
-    <section className="w-full space-y-8 px-6 md:space-y-16">
+    <section className="w-full space-y-8 md:space-y-16">
       <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {Array.from({ length: 17 }).map((_, index: number) => (
           <div
-            className="flex h-33.5 flex-col gap-6 rounded-xl border-b-1 border-b-zinc-500 py-6"
+            className="flex h-33.5 flex-col gap-6 rounded-xl border-b-1 border-b-zinc-500 py-6 shadow-sm"
             key={index}
           >
             <div className="flex gap-4 px-6">
-              <Skeleton className="size-10 rounded-md bg-zinc-800" />
+              <Skeleton className="size-10 rounded-md bg-[var(--skeleton)]" />
               <div className="flex w-full flex-col justify-center gap-2.5">
-                <Skeleton className="h-4 w-full rounded-full bg-zinc-800" />
-                <Skeleton className="h-3 w-1/5 rounded-full bg-zinc-800" />
+                <Skeleton className="h-4 w-full rounded-full bg-[var(--skeleton)]" />
+                <Skeleton className="h-3 w-1/5 rounded-full bg-[var(--skeleton)]" />
               </div>
             </div>
 
             <div className="flex justify-between gap-2 px-6">
-              <Skeleton className="h-5 w-12.5 rounded-md bg-zinc-800" />
-              <Skeleton className="h-5 w-15.5 rounded-md bg-zinc-800" />
+              <Skeleton className="h-5 w-12.5 rounded-md bg-[var(--skeleton)]" />
+              <Skeleton className="h-5 w-15.5 rounded-md bg-[var(--skeleton)]" />
             </div>
           </div>
         ))}
--- src/app/@projects/_components/loading-skeleton.tsx
+++ src/app/@projects/_components/loading-skeleton.tsx
@@ -7,38 +7,38 @@ export function LoadingSkeleton() {
     <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
       {Array.from({ length: 13 }).map((_, index: number) => (
         <div
-          className="flex h-73.5 flex-col gap-6 rounded-xl border-b-1 border-b-zinc-500 py-6"
+          className="flex h-73.5 flex-col gap-6 rounded-xl border-b-1 border-b-zinc-500 py-6 shadow-sm"
           key={index}
         >
           <div className="flex flex-col gap-6 px-6">
-            <Skeleton className="h-4 w-full rounded-full bg-zinc-800" />
+            <Skeleton className="h-4 w-full rounded-full bg-[var(--skeleton)]" />
 
             <div className="flex flex-col gap-4">
               <div className="flex gap-2">
-                <Skeleton className="h-4 w-1/4 rounded-full bg-zinc-800" />
-                <Skeleton className="h-4 w-1/4 rounded-full bg-zinc-800" />
-                <Skeleton className="h-4 w-3/4 rounded-full bg-zinc-800" />
-                <Skeleton className="h-4 w-1/4 rounded-full bg-zinc-800" />
+                <Skeleton className="h-4 w-1/4 rounded-full bg-[var(--skeleton)]" />
+                <Skeleton className="h-4 w-1/4 rounded-full bg-[var(--skeleton)]" />
+                <Skeleton className="h-4 w-3/4 rounded-full bg-[var(--skeleton)]" />
+                <Skeleton className="h-4 w-1/4 rounded-full bg-[var(--skeleton)]" />
               </div>
 
               <div className="flex gap-2">
-                <Skeleton className="h-4 w-3/4 rounded-full bg-zinc-800" />
-                <Skeleton className="h-4 w-2/4 rounded-full bg-zinc-800" />
-                <Skeleton className="h-4 w-1/4 rounded-full bg-zinc-800" />
-                <Skeleton className="h-4 w-1/4 rounded-full bg-zinc-800" />
+                <Skeleton className="h-4 w-3/4 rounded-full bg-[var(--skeleton)]" />
+                <Skeleton className="h-4 w-2/4 rounded-full bg-[var(--skeleton)]" />
+                <Skeleton className="h-4 w-1/4 rounded-full bg-[var(--skeleton)]" />
+                <Skeleton className="h-4 w-1/4 rounded-full bg-[var(--skeleton)]" />
               </div>
             </div>
           </div>
 
           <div className="flex flex-col gap-3 px-6">
-            <Skeleton className="h-4 w-full rounded-full bg-zinc-800" />
-            <Skeleton className="h-4 w-full rounded-full bg-zinc-800" />
-            <Skeleton className="h-4 w-1/3 rounded-full bg-zinc-800" />
+            <Skeleton className="h-4 w-full rounded-full bg-[var(--skeleton)]" />
+            <Skeleton className="h-4 w-full rounded-full bg-[var(--skeleton)]" />
+            <Skeleton className="h-4 w-1/3 rounded-full bg-[var(--skeleton)]" />
           </div>
 
           <div className="flex justify-between gap-2 px-6">
-            <Skeleton className="h-9 w-[94.55px] rounded-md bg-zinc-800" />
-            <Skeleton className="h-9 w-21.5 rounded-md bg-zinc-800" />
+            <Skeleton className="h-9 w-[94.55px] rounded-md bg-[var(--skeleton)]" />
+            <Skeleton className="h-9 w-21.5 rounded-md bg-[var(--skeleton)]" />
           </div>
         </div>
       ))}
