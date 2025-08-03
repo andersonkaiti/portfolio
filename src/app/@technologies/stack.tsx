5bc19cf1d16cbfa78ebae55d6fe54f7eb74674ef
andersonkaiti <anderkaiti@gmail.com>
2025-08-02 22:33:51 -0300

    :lipstick: style: update Stack component layout and styling for improved interactivity and visual appeal

--- src/app/@technologies/stack.tsx
+++ src/app/@technologies/stack.tsx
@@ -4,29 +4,27 @@ import type { IStack } from 'types/stack'
 
 export function Stack({ category, icon: Icon, technologies }: IStack) {
   return (
-    <div className="rounded-md bg-transparent from-zinc-700 to-zinc-700/40 p-[1px] dark:bg-gradient-to-b">
-      <Card className="h-full rounded-md border-0 bg-background shadow-none">
-        <CardHeader className="flex flex-col items-center justify-center gap-6">
+    <Card className="group h-full cursor-pointer bg-background hover:bg-card/20">
+      <CardHeader className="flex flex-col items-center justify-center gap-6">
+        <Badge
+          className="rounded-full p-4 shadow-emerald-500/50 shadow-lg transition-colors duration-300 group-hover:border-emerald-500"
+          variant="outline"
+        >
+          <Icon className="!size-10" />
+        </Badge>
+        <CardTitle>{category}</CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-wrap gap-2">
+        {technologies.map((tech) => (
           <Badge
-            className="rounded-full p-4 shadow-emerald-500/50 shadow-lg"
+            className="border-emerald-500/40 bg-emerald-500/10 text-emerald-500"
+            key={tech}
             variant="outline"
           >
-            <Icon className="!size-10" />
+            {tech}
           </Badge>
-          <CardTitle>{category}</CardTitle>
-        </CardHeader>
-        <CardContent className="flex flex-wrap gap-2">
-          {technologies.map((tech) => (
-            <Badge
-              className="border-emerald-500/40 bg-emerald-500/10 text-emerald-500"
-              key={tech}
-              variant="outline"
-            >
-              {tech}
-            </Badge>
-          ))}
-        </CardContent>
-      </Card>
-    </div>
+        ))}
+      </CardContent>
+    </Card>
   )
 }
