f097a67fff1bb1b731df71527c2b126f3b26a2aa
andersonkaiti <anderkaiti@gmail.com>
2025-08-02 22:34:30 -0300

    :lipstick: style: refactor CertificationList component layout by replacing Card with a div

--- src/app/@certifications/_components/certification-list.tsx
+++ src/app/@certifications/_components/certification-list.tsx
@@ -1,5 +1,4 @@
 import { Badge } from '@components/ui/badge'
-import { Card, CardContent } from '@components/ui/card'
 import { getCourses } from '@services/courses.service'
 import { Award } from 'lucide-react'
 import { Certification } from './certification'
@@ -14,22 +13,20 @@ export async function CertificationList() {
           <Certification key={course.id} {...course} />
         ))}
       </div>
-      <Card className="mx-auto w-full items-center bg-transparent text-center md:w-fit md:text-start">
-        <CardContent className="flex flex-col items-center gap-4 md:flex-row">
-          <Badge className="aspect-square h-fit border-emerald-500/20 bg-emerald-500/10 p-2">
-            <Award className="!size-8 text-emerald-500" />
-          </Badge>
-          <div>
-            <h1 className="scroll-m-20 text-balance font-bold text-2xl tracking-tight">
-              Total de horas
-            </h1>
-            <h3 className="scroll-m-20 font-bold text-3xl text-emerald-500 text-shadow-emerald-500/20 text-shadow-lg tracking-tight">
-              {totalHours} HORAS
-            </h3>
-            <footer>de cursos e certificações</footer>
-          </div>
-        </CardContent>
-      </Card>
+      <div className="mx-auto flex w-full flex-col items-center gap-4 bg-background text-center md:w-fit md:flex-row md:text-start">
+        <Badge className="aspect-square h-fit border-emerald-500/20 bg-emerald-500/10 p-2">
+          <Award className="!size-8 text-emerald-500" />
+        </Badge>
+        <div>
+          <h1 className="scroll-m-20 text-balance font-bold text-2xl tracking-tight">
+            Total de horas
+          </h1>
+          <h3 className="scroll-m-20 font-bold text-3xl text-emerald-500 text-shadow-emerald-500/20 text-shadow-lg tracking-tight">
+            {totalHours} HORAS
+          </h3>
+          <footer>de cursos e certificações</footer>
+        </div>
+      </div>
     </>
   )
 }
