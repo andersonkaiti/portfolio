b9f953c96df79078c88858d5bbfd4c769a6fa70e
andersonkaiti <anderkaiti@gmail.com>
2025-08-09 18:47:41 -0300

    :lipstick: style(education): update layout with SectionUnderline, refine Card styles, and improve text readability

--- src/app/@education/page.tsx
+++ src/app/@education/page.tsx
@@ -11,6 +11,7 @@ import {
   SectionHeader,
   SectionSubtitle,
   SectionTitle,
+  SectionUnderline,
 } from '@components/ui/section'
 import { Globe, GraduationCap } from 'lucide-react'
 
@@ -22,11 +23,12 @@ export default function Education() {
         <SectionSubtitle>
           Minha educação formal e desenvolvimento de idiomas
         </SectionSubtitle>
+        <SectionUnderline />
       </SectionHeader>
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <Card className="bg-background transition-colors duration-300 hover:border-emerald-500 hover:bg-card/20">
-          <CardHeader className="flex gap-2">
+        <Card className="bg-background">
+          <CardHeader className="relative flex gap-2">
             <Badge className="border-emerald-500/20 bg-emerald-500/10 p-2">
               <GraduationCap className="!size-10 text-emerald-500" />
             </Badge>
@@ -38,24 +40,28 @@ export default function Education() {
                 UNISAGRADO • 2022-2025
               </h4>
             </div>
+
+            <div className="absolute inset-0 top-2 size-full rounded-xl bg-gradient-to-b from-background/0 to-background" />
           </CardHeader>
-          <CardContent className="space-y-4">
-            <CardTitle>Ciência da Computação</CardTitle>
-            <p>
+
+          <CardContent className="relative z-1 space-y-1">
+            <CardTitle className="text-base">CIÊNCIA DA COMPUTAÇÃO</CardTitle>
+            <p className="font-light text-muted-foreground text-sm">
               Cursando Ciência da Computação no UNISAGRADO, com término previsto
               para 2025. Focado em desenvolvimento de software, algoritmos e
               estruturas de dados.
             </p>
           </CardContent>
-          <CardFooter>
+
+          <CardFooter className="z-1">
             <Badge className="rounded-full border-emerald-500/20 bg-emerald-500/10 text-emerald-500">
               Formatura: 2025
             </Badge>
           </CardFooter>
         </Card>
 
-        <Card className="bg-background transition-colors duration-300 hover:border-emerald-500 hover:bg-card/20">
-          <CardHeader className="flex gap-2">
+        <Card className="bg-background">
+          <CardHeader className="relative flex gap-2">
             <Badge className="border-emerald-500/20 bg-emerald-500/10 p-2">
               <Globe className="!size-10 text-emerald-500" />
             </Badge>
@@ -67,18 +73,22 @@ export default function Education() {
                 CNA • Em andamento
               </h4>
             </div>
+
+            <div className="absolute inset-0 top-2 size-full rounded-xl bg-gradient-to-b from-background/0 to-background" />
           </CardHeader>
-          <CardContent className="space-y-4">
-            <CardTitle>Inglês</CardTitle>
-            <p>
+
+          <CardContent className="z-1 space-y-1">
+            <CardTitle className="text-base">INGLÊS</CardTitle>
+            <p className="font-light text-muted-foreground text-sm">
               Cursando o 3º ano do curso de inglês da CNA, desenvolvendo
               habilidades de comunicação para o ambiente profissional
               internacional.
             </p>
           </CardContent>
-          <CardFooter>
-            <Badge className="rounded-full border-emerald-500/20 bg-emerald-500/10 text-emerald-500">
-              Nível: Pré-avançado
+
+          <CardFooter className="z-1 h-full">
+            <Badge className="mt-auto rounded-full border-emerald-500/20 bg-emerald-500/10 text-emerald-500">
+              Nível: Pré-Avançado
             </Badge>
           </CardFooter>
         </Card>
