b73232136f47c63c24766a30d778c03373f32036
andersonkaiti <anderkaiti@gmail.com>
2025-09-13 20:37:03 -0300

    :fire: refactor(dark-mode-button): simplify theme toggling by removing direct theme management

--- src/app/_components/dark-mode-button.tsx
+++ src/app/_components/dark-mode-button.tsx
@@ -1,21 +1,9 @@
 import { AnimatedThemeToggler } from '@components/magicui/animated-theme-toggler'
 import { Button } from '@components/ui/button'
-import { useTheme } from 'next-themes'
 
 export function DarkModeButton() {
-  const { theme, setTheme } = useTheme()
-
-  function toggleTheme() {
-    setTheme(theme === 'dark' ? 'light' : 'dark')
-  }
-
   return (
-    <Button
-      asChild
-      className="z-50 ml-auto size-9"
-      onClick={toggleTheme}
-      variant="ghost"
-    >
+    <Button asChild className="z-50 ml-auto size-9" variant="ghost">
       <AnimatedThemeToggler />
     </Button>
   )
