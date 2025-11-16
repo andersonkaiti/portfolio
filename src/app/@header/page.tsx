bd0dab829926f4ff5ff2143c0791d365db8f1fb8
andersonkaiti <anderkaiti@gmail.com>
2025-11-16 19:16:00 -0300

    :fire: remove(code): delete unused Code component from header and simplify structure, and improve UI clarity

--- src/app/@header/page.tsx
+++ src/app/@header/page.tsx
@@ -1,126 +1,65 @@
-import { Avatar, AvatarImage } from '@components/ui/avatar'
-import { Badge } from '@components/ui/badge'
-import { Button } from '@components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@components/ui/card'
-import {
-  Cpu,
-  FileUser,
-  Github,
-  Linkedin,
-  MapPin,
-  MessageCircle,
-} from 'lucide-react'
+import { FileUser, Github, Linkedin, MessageCircle } from 'lucide-react'
 import Link from 'next/link'
-import { Code } from './code'
 
 export default function Header() {
   return (
     <header
-      className="grid grid-cols-1 items-center gap-4 xl:grid-cols-2"
+      className="mx-2 my-10 flex flex-col gap-6 text-center sm:mx-8 sm:my-16 md:mx-20 md:my-24 lg:mx-24 lg:my-24 "
       id="presentation"
     >
-      <Card
-        className="border-0 bg-background shadow-none"
-        data-aos="fade-down"
-        data-aos-delay={200}
-      >
-        <CardHeader className="flex flex-col items-center gap-4 md:flex-row">
-          <Avatar className="size-30 ring-3 ring-emerald-500/20">
-            <AvatarImage src="/foto.webp" />
-          </Avatar>
-          <CardTitle className="flex flex-col items-center md:items-start">
-            <div className="flex items-center gap-2">
-              <Cpu className="size-5 text-emerald-500" />
-              <Badge
-                className="rounded-full border-emerald-500/50 bg-emerald-500/10 text-emerald-500 shadow-emerald-500/20 shadow-lg"
-                variant="outline"
-              >
-                Cientista da Computação
-              </Badge>
-            </div>
+      <div className="relative mx-auto w-fit">
+        <div className="-top-4 -translate-x-1/2 absolute left-1/2 flex items-center gap-2 sm:gap-3">
+          <div className="size-2 animate-pulse rounded-full bg-green-500 sm:size-3" />
+          <span className="font-medium text-green-700 text-xs sm:text-sm dark:text-green-400">
+            Available for work
+          </span>
+        </div>
 
-            <h1 className="scroll-m-20 text-balance break-words text-start font-extrabold text-4xl tracking-wide">
-              Anderson Kaiti
-            </h1>
+        <h1 className="scroll-m-20 text-balance text-center font-base text-3xl xs:text-4xl tracking-tight sm:text-5xl md:text-7xl ">
+          Hi, I'm Anderson Kaiti
+        </h1>
+      </div>
 
-            <span className="mt-2 flex gap-1 font-light text-emerald-500">
-              <MapPin className="size-4" /> Bauru, SP - Brasil
-            </span>
-          </CardTitle>
-        </CardHeader>
+      <h2 className="font-semibold text-lg text-primary sm:text-xl md:text-2xl">
+        Full-Stack Web Developer
+      </h2>
 
-        <CardContent className="text-justify font-light text-lg">
-          <h3 className="scroll-m-20 font-semibold text-2xl text-emerald-500 tracking-tight">
-            Desenvolvedor Web Full-Stack
-          </h3>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            Discente de Ciência da Computação com foco em desenvolvimento web,
-            tenho domínio em{' '}
-            <strong className="text-emerald-500">
-              HTML5, CSS3, JavaScript, TypeScript, Node.js, MySQL, React.js,
-              Next.js, TailwindCSS, Prisma, Firebase, Git e Docker
-            </strong>
-            .
-          </p>
-        </CardContent>
+      <p className="mx-auto mt-3 max-w-2xl text-balance font-base text-base leading-6 sm:mt-4 sm:text-lg sm:leading-7 md:text-xl md:leading-8 ">
+        Computer Science student focused on web development, with experience in
+        modern technologies like React, Next.js, Node.js, TypeScript,
+        PostgreSQL, TailwindCSS, Docker and others.
+      </p>
 
-        <CardFooter className="flex flex-wrap gap-2 sm:gap-4">
-          <Link
-            className="w-full sm:w-fit"
-            href="https://github.com/andersonkaiti"
-            target="_blank"
-          >
-            <Button
-              className="w-full transition-all duration-300 hover:shadow-emerald-500/40 hover:shadow-lg"
-              variant="outline"
-            >
-              <Github /> GitHub
-            </Button>
-          </Link>
-          <Link
-            className="w-full sm:w-fit"
-            href="https://www.linkedin.com/in/anderson-kaiti-67906126a"
-            target="_blank"
-          >
-            <Button
-              className="w-full transition-all duration-300 hover:shadow-emerald-500/40 hover:shadow-lg"
-              variant="outline"
-            >
-              <Linkedin /> LinkedIn
-            </Button>
-          </Link>
-          <Link
-            className="w-full sm:w-fit"
-            href="https://wa.me/14998053657"
-            target="_blank"
-          >
-            <Button
-              className="w-full transition-all duration-300 hover:shadow-emerald-500/40 hover:shadow-lg"
-              variant="outline"
-            >
-              <MessageCircle /> WhatsApp
-            </Button>
-          </Link>
-          <Link
-            className="w-full sm:w-fit"
-            href="/anderson-kaiti-curriculo.pdf"
-            target="_blank"
-          >
-            <Button className="w-full border border-emerald-300 bg-emerald-500 shadow-emerald-500/40 shadow-lg transition-all duration-300 hover:shadow-emerald-500/60">
-              <FileUser />
-              Currículo
-            </Button>
-          </Link>
-        </CardFooter>
-      </Card>
-
-      <Code />
+      <div className="mx-auto mt-8 flex flex-wrap items-center justify-center gap-5 text-muted-foreground sm:mt-10 sm:gap-8 ">
+        <Link
+          className="transition duration-500 hover:scale-125"
+          href="https://github.com/andersonkaiti"
+          target="_blank"
+        >
+          <Github className="size-5 sm:size-6" />
+        </Link>
+        <Link
+          className="transition duration-500 hover:scale-125"
+          href="https://www.linkedin.com/in/anderson-kaiti-67906126a"
+          target="_blank"
+        >
+          <Linkedin className="size-5 sm:size-6" />
+        </Link>
+        <Link
+          className="transition duration-500 hover:scale-125"
+          href="https://wa.me/14998053657"
+          target="_blank"
+        >
+          <MessageCircle className="size-5 sm:size-6" />
+        </Link>
+        <Link
+          className="transition duration-500 hover:scale-125"
+          href="/anderson-kaiti-curriculo.pdf"
+          target="_blank"
+        >
+          <FileUser className="size-5 sm:size-6" />
+        </Link>
+      </div>
     </header>
   )
 }
