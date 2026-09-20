2d95711ce9f67bba71f43d406997022e31e6ddd6
Anderson Kaiti <anderkaiti@gmail.com>
2026-09-19 22:29:19 -0300

    🔧 chore(commitlint): move configs to package.json and drop @commitlint/types

--- commitlint.config.ts
+++
@@ -1,7 +0,0 @@
-import type { UserConfig } from '@commitlint/types'
-
-const configuration: UserConfig = {
-  extends: ['@commitlint/config-conventional', 'gitmoji'],
-}
-
-export default configuration
